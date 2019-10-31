59febd9422f9a03be7a67efa2d2196160518ed8e
Priyesh <49675125+priyesh014@users.noreply.github.com>
2019-10-31 21:22:47 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/ui/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NoToDo',
+      home: new Home(),
+    );
+  }
+}
